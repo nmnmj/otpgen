8e4cfcb65cbc817c9d077b51484f29dd53f3bcac
nmnmj <nmnjay@gmail.com>
2023-03-05 18:19:40 +0530

    d

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.use("/", web)
+app.use("/", (req,res)=>{
+    res.render("index.ejs")
+})
+// app.use("/", web)
 const port = process.env.PORT
 
 
